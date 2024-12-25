296e2e0d07a5be1e86b60671ddb3359a4bcef68a
berinay.sariel <berinay.sariel@sabanciuniv.edu>
2024-12-25 14:58:11 +0300

    Update processing_insta_data.py

--- scripts/processing_insta_data.py
+++ scripts/processing_insta_data.py
@@ -1,36 +1,23 @@
-import pandas as pd
 import json
-import os
-from datetime import datetime, timezone, timedelta
+import pandas as pd
+from datetime import datetime, timezone
 
-output_dir = 'processed_data_insta'
-os.makedirs(output_dir, exist_ok=True)
-
-file_path = '/Users/berinayzumrasariel/Desktop/DSA210 TERM PROJECT/liked_posts.json'
-with open(file_path, 'r') as file:
+raw_file_path = '/Users/berinayzumrasariel/Desktop/DSA210 TERM PROJECT/liked_posts.json'
+with open(raw_file_path, 'r') as file:
     data = json.load(file)
-    
+
 timestamps = []
 for like in data['likes_media_likes']:
-    for item in like["string_list_data"]:
-        timestamp = int(item['timestamp'])
-        timestamps.append(timestamp)
+    for item in like['string_list_data']:
 
-df_instagram = pd.DataFrame({'timestamp': timestamps})
-df_instagram['timestamp'] = pd.to_datetime(df_instagram['timestamp'], unit='s')  
-df_instagram['date'] = df_instagram['timestamp'].dt.date 
-df_instagram['time'] = df_instagram['timestamp'].dt.strftime('%H:%M')  
+        utc_timestamp = int(item['timestamp'])
+        dt_utc = datetime.fromtimestamp(utc_timestamp, tz=timezone.utc)
+        timestamps.append(dt_utc)
 
-processed_data = df_instagram[['date', 'time']]
+likes_df = pd.DataFrame({'timestamp': timestamps})
+likes_df['timestamp'] = likes_df['timestamp'].dt.tz_convert('Europe/Istanbul')
+likes_df['hour'] = likes_df['timestamp'].dt.hour
 
-processed_data['date'] = processed_data['date'].astype(str)
-
-csv_output_path = os.path.join(output_dir, 'processed_instagram.csv')
-processed_data.to_csv(csv_output_path, index=False)
-
-json_output_path = os.path.join(output_dir, 'processed_instagram.json')
-processed_data_json = processed_data.to_dict(orient='records')  # Convert DataFrame to list of dictionaries
-with open(json_output_path, 'w') as json_file:
-    json.dump(processed_data_json, json_file, indent=4)
-
-print(f"Instagram data has been processed and saved to:\nCSV: {csv_output_path}\nJSON: {json_output_path}")
+processed_file_path = '/Users/berinayzumrasariel/Desktop/DSA210 TERM PROJECT/processed_likes.csv'
+likes_df.to_csv(processed_file_path, index=False)
+print(f"Preprocessed data saved to {processed_file_path}")
