60001e3d7633c24f16b1fcf89168199d8c7aa2d2
berinay.sariel <berinay.sariel@sabanciuniv.edu>
2024-12-25 18:04:13 +0300

    Update study hours vs instagram usage.py

--- scripts/study hours vs instagram usage.py
+++ scripts/study hours vs instagram usage.py
@@ -1,19 +1,25 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
 study_file_path = "/Users/berinayzumrasariel/Desktop/DSA210 TERM PROJECT/Cleaned_and_Adjusted_Study_Data.csv"
-instagram_file_path = "/Users/berinayzumrasariel/Desktop/DSA210 TERM PROJECT/extracted_likes_timestamps.csv"
+instagram_file_path = "/Users/berinayzumrasariel/Desktop/DSA210 TERM PROJECTcontent/processed_likes.csv"
+
+output_directory = "/Users/berinayzumrasariel/Desktop/DSA210 TERM PROJECT/insta_usage_vs_study_graphs"
+os.makedirs(output_directory, exist_ok=True)
 
 study_data = pd.read_csv(study_file_path)
+instagram_data = pd.read_csv(instagram_file_path)
+
 study_data["Start Time"] = pd.to_datetime(study_data["Start Time"], errors="coerce").dt.tz_localize(None)
 study_data["End Time"] = pd.to_datetime(study_data["End Time"], errors="coerce").dt.tz_localize(None)
 study_data = study_data.dropna(subset=["Start Time", "End Time"])
 study_data["Hour"] = study_data["Start Time"].dt.hour
 
-instagram_data = pd.read_csv(instagram_file_path, header=None, names=["timestamp"])
 instagram_data["timestamp"] = pd.to_datetime(instagram_data["timestamp"], errors="coerce").dt.tz_localize(None)
 instagram_data = instagram_data.dropna(subset=["timestamp"])
+
 
 academic_periods = {
     "Feb 2023 - June 2023": (pd.Timestamp("2023-02-01"), pd.Timestamp("2023-06-30")),
@@ -21,8 +27,6 @@ academic_periods = {
     "Feb 2024 - June 2024": (pd.Timestamp("2024-02-01"), pd.Timestamp("2024-06-30")),
     "Oct 2024 - Now": (pd.Timestamp("2024-10-01"), instagram_data["timestamp"].max()),
 }
-
-output_directory = "/Users/berinayzumrasariel/Desktop/DSA210 TERM PROJECT"
 
 for period_name, (start_date, end_date) in academic_periods.items():
     period_study_data = study_data[
